b0fd33fb80b314e449ffd6884c2fcf4318a1818d
practiceRepoG <kavyar101@gmail.com>
2021-01-27 10:38:28 +0530

    recursion implementation

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ class CellState {
         this.cellYPos = cellYPos;
         this.cellVal = "clear";
         this.cellClicked = false;
+        this.cellBmbCount = 0;
     }
 }
 
@@ -34,7 +35,7 @@ function initialiseGrid() {
 //Assigning boms randomly as per user input
 function assignBombRandomly(bombCount) {
     let randomeCell;
-    for (let i = 0; i <= bombCount; i++) {
+    for (let i = 0; i < bombCount; i++) {
         randomeCell = Math.floor(Math.random() * (gridRowBkp * gridRowBkp));
         myGrid[randomeCell].cellVal = "bomb";
     }
@@ -48,7 +49,7 @@ function updateDiv() {
     while (i < gridRowBkp) {
         str += "<tr>";
         while (j < gridColBkp) {
-            str += `<th id="${i}-${myGrid[j].cellYPos}-${k}"></th>`;
+            str += `<th id="${i}-${myGrid[j].cellYPos}-${k}" style="height:100px;width:100px"></th>`;
             j++;
             k++;
         }
@@ -59,45 +60,73 @@ function updateDiv() {
     document.getElementById("gridBox").innerHTML = str;
 }
 
-
-function cellClicked(e) {
-    let idInfo = e.target.id.toString().split("-");
-    console.log(myGrid[idInfo[2]]);
-    if (myGrid[idInfo[2]].cellVal !== "bomb") {
-        document.getElementById(e.target.id).style.background = "green";
-        myGrid[idInfo[2]].cellClicked = true;
-        document.getElementById(e.target.id).innerText = checkAdjCells(idInfo[2]);
-    }
-    else {
-        document.getElementById(e.target.id).style.background = "red";
-        document.getElementById("gridBox").innerHTML = "<h2>You Lost..!!</h2>";
-    }
-}
-
 //To check adjacent cell
 const adj = [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]];
+let tmpAdj = [], nextPos;
 
-function checkAdjCells(pos) {
-    let cellXPos = myGrid[pos].cellXPos;
-    let cellYPos = myGrid[pos].cellYPos;
-    let tmpAdj = [], tmpX, tmpY;
-    console.log(cellXPos + adj[0][0]);
-    for (let i = 0; i < adj.length; i++) {
-        tmpX = cellXPos + adj[i][0];
-        tmpY = cellYPos + adj[i][1];
-        tmpAdj.push([tmpX, tmpY]);
+function cellClicked(e) {
+    nextPos = [];
+    let idInfo = e.target.id.toString().split("-");
+    console.log(myGrid[idInfo[2]]);
+    if (myGrid[idInfo[2]].cellVal !== "bomb" && myGrid[idInfo[2]].cellClicked !== true) {
+        document.getElementById(e.target.id).style.background = "green";
+        myGrid[idInfo[2]].cellClicked = true;
+        // document.getElementById(e.target.id).innerText = checkAdjCells(idInfo[2]);
+        checkAdjCells(idInfo[2]);
     }
-   return countTheBomb(tmpAdj);
+    else if(myGrid[idInfo[2]].cellVal === "bomb"){
+        document.getElementById("gridBox").innerHTML = "<h2>You Lost..!!</h2>";
+        document.getElementById("startBtn").style.disable = true;
+    }
 }
 
-function countTheBomb(tmpAdj){
+function checkAdjCells(pos) {
+        let cellXPos = myGrid[pos].cellXPos;
+        let cellYPos = myGrid[pos].cellYPos;
+        let tmpX, tmpY;
+        console.log(cellXPos + adj[0][0]);
+        for (let i = 0; i < adj.length; i++) {
+            tmpX = cellXPos + adj[i][0];
+            tmpY = cellYPos + adj[i][1];
+            if (tmpX < gridColBkp && tmpX >= 0 && tmpY < gridColBkp && tmpY >= 0)
+                tmpAdj.push([tmpX, tmpY]);
+        }
+     countTheBomb(tmpAdj, pos);
+}
+
+function countTheBomb(tmpAdjs, pos) {
     let count = 0;
-    for(let i=0; i<myGrid.length; i++){
-        for(let j=0; j<tmpAdj.length; j++){
-            if(myGrid[i].cellXPos === tmpAdj[j][0] && myGrid[i].cellYPos === tmpAdj[j][1] && myGrid[i].cellVal === "bomb"){
-                count++;
+    for (let i = 0; i < myGrid.length; i++) {
+        for (let j = 0; j < tmpAdjs.length; j++) {
+            if (myGrid[i].cellXPos === tmpAdjs[j][0] && myGrid[i].cellYPos === tmpAdjs[j][1]) {
+                if (myGrid[i].cellVal === "bomb")
+                    count++;
+                if(myGrid[i].cellClicked!== true && myGrid[i].cellBmbCount===0)
+                    nextPos.push(i);
+                if(myGrid[i].cellClicked=== true){
+                    document.getElementById(`${myGrid[i].cellXPos}-${myGrid[i].cellYPos}-${i}`).style.background = "green";
+                    if(myGrid[i].cellBmbCount>0)
+                        document.getElementById(`${myGrid[i].cellXPos}-${myGrid[i].cellYPos}-${i}`).innerText = myGrid[i].countTheBomb.toString();
+                }
             }
         }
     }
-    return count;
+    if (count > 0) {
+        tmpAdj = [];
+        myGrid[pos].cellBmbCount = count;
+        document.getElementById(`${myGrid[pos].cellXPos}-${myGrid[pos].cellYPos}-${pos}`).style.background = "green";
+        document.getElementById(`${myGrid[pos].cellXPos}-${myGrid[pos].cellYPos}-${pos}`).innerText = count;
+        return;
+    }
+    else {
+        nextPos = [...new Set(nextPos)];
+        while (nextPos.length) {
+            tmpAdj = [];
+            if(myGrid[nextPos[nextPos.length - 1]].cellClicked !== true){
+                myGrid[nextPos[nextPos.length - 1]].cellClicked = true;
+                let p = nextPos.shift();
+                checkAdjCells(p);
+            }
+        }
+    }
 }
